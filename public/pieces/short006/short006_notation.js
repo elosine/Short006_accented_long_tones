c04bb026ddc7f6f876a503e1191d9ae754e19f56
elosine <justinwenloyang@gmail.com>
2023-11-22 18:07:01 -0500

    u

--- public/pieces/short006/short006_notation.js
+++ public/pieces/short006/short006_notation.js
@@ -1,32 +1,36 @@
 //#ef NOTES
 /*
-Make several pages of curves and have  a random trigger at the end of a Staff
-Slow cursor
-Get back to framerate timer
+Generate Curve Upstroke
+Make 1 staff of Curves
+Make curves for all staves
 
-2 Pages for every Staff
-trigger non active staff at last pixel/ms for the staff use modulo
-Fix staff width at 1260
-Generate a databse of pages for each staff and curve locations
-length of curve grows at different rate for each staff then shrinks
-gap between curves for each staff different pace
-Animate Cursor using timelineMS
-Array of timeline for each ms which pixel
-turn each pixel into MS
-tIMING:
-clock time
+Rethink the Timing Clock
+  Curve will last x number of frames and begin on a certain frame
+  Each frame in curve life will have an x and y location
+    Generate Curve
+    Calculate curve start as frame number
+    Calculate duration of curve in number of frames
+    Use number of pixels per frame to find curve x index and look up y and store coordinates
 
-what is last horizontal pixel?
-Animate one scrolling line at continuous tempo
-each staff as own svgcontainer
 
-ANIMATE CURSOR
-px per ms
+Draw curve with new timeline
 
-Print Epoch Time
-Draw Scrolling cursors
-Position Scrolling Cursors
-Implement Animation Engine
+Shore up timeline:
+  Base counter will be frames
+  Array with the total number of frames in a timeline
+  Each index (i.e. frame) will contain:
+    elapsed time in MS
+    X location in pixels
+
+Make one curve follower and test curve amount and width/duration
+Array the same length as timelineFRAMES for Curves
+  Generate 2 Curves: onset time; descent exponent; ascent exponent
+  look at current curve
+  Each curve has its Y location in this array
+  Otherwise at top
+Fix Animation Engine for Scrolling Cursors to Frame clock
+Curve Follower from sf004
+Doesn't need to be perfect just needs to run
 */
 //#endef NOTES
 
@@ -39,12 +43,14 @@ const TEMPO_COLORS = [clr_brightOrange, clr_brightGreen, clr_brightBlue, clr_lav
 
 //##ef Timing
 const FRAMERATE = 60;
+const FRAMES_PER_MS = FRAMERATE / 1000;
 let FRAMECOUNT = 0;
-const PX_PER_SEC = 40;
+const PX_PER_SEC = 30; //scrolling speed
 const PX_PER_MS = PX_PER_SEC / 1000;
 const MS_PER_PX = 1000 / PX_PER_SEC;
 const PX_PER_FRAME = PX_PER_SEC / FRAMERATE;
 const MS_PER_FRAME = 1000.0 / FRAMERATE;
+
 //##endef Timing
 
 //#ef Animation Engine Variables
@@ -79,29 +85,53 @@ const NOTATIONCANVAS_H = WORLD_H;
 const NOTATIONCANVAS_W = WORLD_W;
 //#endef Canvas Variables
 
+//#ef Scrolling Bouncy Balls
+let scrBBs = [];
+const scrBB_RADIUS = 7;
+//#endef Scrolling Bouncy Balls
+
 //#ef Staff Variables
 const NUMSTAVES = 4;
 const STAFFGAP = 4;
 const STAFF_H = (NOTATIONCANVAS_H - (STAFFGAP * (NUMSTAVES - 1))) / NUMSTAVES;
 const STAFF_W = NOTATIONCANVAS_W;
-console.log(STAFF_W);
 let staves = [];
 //#endef Staff Variables
 
 //#ef Staff Timing
-const STAFF_W_MS = Math.round(STAFF_W * MS_PER_PX);
-let timelineMS = [];
-for (var i = 0; i < STAFF_W_MS; i++) {
-  let tpx = Math.round(i * PX_PER_MS);
-  timelineMS.push(tpx);
+//find out how many frames in a timeline
+const STAFF_WIDTH_IN_FRAMES = Math.round(STAFF_W / PX_PER_FRAME); //Width of the staff in frames
+let timelineFRAMES = [];
+//which frame for each MS
+const STAFF_WIDTH_IN_MS = Math.round(STAFF_W / PX_PER_MS); //Width of the staff in frames
+let timelineMS_returnsFrames = [];
+for (var i = 0; i < STAFF_WIDTH_IN_MS; i++) {
+  let tFrameNum = Math.round(FRAMES_PER_MS * i);
+  timelineMS_returnsFrames.push(tFrameNum);
+}
+
+//Should contain an index for each frame in the timeline
+for (var i = 0; i < STAFF_WIDTH_IN_FRAMES; i++) {
+  let tFrameDict = {}; //a dictionary for each frame
+  let tpx = Math.round(i * PX_PER_FRAME); //this is the pixel location for each frame
+  tFrameDict['x'] = tpx;
+  let tMS = Math.round(i * MS_PER_FRAME);
+  tFrameDict['ms'] = tMS;
+  tFrameDict['crvY'] = scrBB_RADIUS;
+  timelineFRAMES.push(tFrameDict);
 }
 //#endef Staff Timing
 
 //#ef Scrolling Tempo Cursors
 let scrollingCursors = [];
-const NOTATION_CURSOR_H = STAFF_H;
-const NOTATION_CURSOR_STROKE_W = 3;
 //#endef Scrolling Tempo Cursors
+
+//#ef Curves
+let crvYcoords = [timelineFRAMES.length]; //array with an index for each frame of the timeline which will contain the Y coordinate of the curve;
+for (var i = 0; i < timelineFRAMES.length; i++) { //initialize all with BB at the top;
+  crvYcoords.push(scrBB_RADIUS);
+}
+//#endef Curves
 
 //#endef GLOBAL VARIABLES
 
@@ -111,6 +141,7 @@ function init() {
   makeWorldPanel();
   makeStaves();
   makeScrollingCursors();
+  makeScrollBBs();
   makeCurves();
 
   let ts_Date = new Date(TS.now()); //Date stamp object from TimeSync library
@@ -204,10 +235,9 @@ function makeScrollingCursors() {
       x2: 0,
       y2: 0,
       stroke: TEMPO_COLORS[scrollingCsrIx],
-      strokeW: NOTATION_CURSOR_STROKE_W
+      strokeW: 3
     });
     tLine.setAttributeNS(null, 'stroke-linecap', 'round');
-    tLine.setAttributeNS(null, 'display', 'none');
     // tLine.setAttributeNS(null, 'transform', "translate(" + beatCoords[4].x.toString() + "," + beatCoords[4].y.toString() + ")");
     scrollingCursors.push(tLine);
 
@@ -218,61 +248,128 @@ function makeScrollingCursors() {
 
 //#ef Make Curves
 function makeCurves() {
-  var crvCoords = plot(function(x) {
-    return Math.pow(x, 1.5);
-  }, [0, 1, 1, 0], 15, STAFF_H);
+  //GEnerate start time and duration
+  // start sec 3, duration 1.5 seconds
+  //Calculate # of pixels based on duration
+  let crvWidthPixels = Math.round(1.5 * PX_PER_SEC);
+  //Generate Curve of crvWidthPixels; each index has x and y
+  var crvCoords = plot(function(x) { //generate curve
+    return Math.pow(x, 0.5); //Math.pow(x, 1.5) the 1.5 here is the curve shape; >1 slow curve at beginning, faster curve at end; <1 opposite
+  }, [0, 1, 1, 0], crvWidthPixels, STAFF_H); // [0, 1, 1, 0], 150, STAFF_H) 150 is width of the curve; generates array with size 150
 
+  let crvStartTimeMS = 3000;
+  let crvStartFrameNumber = Math.round(crvStartTimeMS / MS_PER_FRAME); //find out which frame the curve starts on and look up the x-pixel in timelineFrames;
+  let crvDurFrames = Math.round(1.5 * FRAMERATE);
+  //let crvStartX = timelineFRAMES[crvStartFrameNumber].x;
+
+  //add y coordinate of curve to timelineFRAMES
+  // each x convert to framenumber
+  // add start frame number
+  // frame i * frames per pixel = crvCoords ix
+  for (var i = 0; i < crvDurFrames; i++) {
+    let tframeNum = crvStartFrameNumber + i;
+    let tLookUpPixel = Math.min(Math.round(i * PX_PER_FRAME), crvCoords.length - 1);
+    console.log(tLookUpPixel);
+    let tCrvY = crvCoords[tLookUpPixel].y;
+    timelineFRAMES[tframeNum].crvY = tCrvY;
+  }
+
+  //Draw Curve
+  let crvStartX = crvStartTimeMS * PX_PER_MS;
   var tSvgCrv = document.createElementNS(SVG_NS, "path");
   var tpathstr = "";
+
   for (var i = 0; i < crvCoords.length; i++) {
+    let tcrvX = crvStartX + crvCoords[i].x;
     if (i == 0) {
-      tpathstr = tpathstr + "M" + crvCoords[i].x.toString() + " " + crvCoords[i].y.toString() + " ";
+      tpathstr = tpathstr + "M" + tcrvX.toString() + " " + crvCoords[i].y.toString() + " ";
     } else {
-      tpathstr = tpathstr + "L" + crvCoords[i].x.toString() + " " + crvCoords[i].y.toString() + " ";
+      tpathstr = tpathstr + "L" + tcrvX.toString() + " " + crvCoords[i].y.toString() + " ";
     }
   }
+
   tSvgCrv.setAttributeNS(null, "d", tpathstr);
   tSvgCrv.setAttributeNS(null, "stroke", "rgba(255, 21, 160, 0.5)");
   tSvgCrv.setAttributeNS(null, "stroke-width", "4");
   tSvgCrv.setAttributeNS(null, "fill", "none");
-  tSvgCrv.setAttributeNS(null, "transform", "translate( 100, 20)");
+  // tSvgCrv.setAttributeNS(null, "transform", "translate( 100, 20)");
   staves[0].svg.appendChild(tSvgCrv);
+
 }
 //#endef Make Curves
+
+//#ef Make Cursor Bouncy Ball
+function makeScrollBBs() {
+
+  for (let scrollBBsIx = 0; scrollBBsIx < NUM_PLAYERS; scrollBBsIx++) {
+
+    let tBB = mkSvgCircle({
+      svgContainer: staves[scrollBBsIx].svg,
+      cx: 0,
+      cy: scrBB_RADIUS,
+      r: scrBB_RADIUS,
+      fill: TEMPO_COLORS[scrollBBsIx],
+      stroke: 'white',
+      strokeW: 0
+    });
+    scrBBs.push(tBB);
+
+  } // END for (let scrollBBsIx = 0; scrollBBsIx < NUM_PLAYERS; scrollBBsIx++) {
+
+} //function makeScrollBBs() END
+//#endef Make Cursor Bouncy Ball
+
+//#ef Make Cursor Bouncy Ball Curve Follower
+function makeBBcrvFollower() {
+
+  for (let scrollBBsIx = 0; scrollBBsIx < NUM_PLAYERS; scrollBBsIx++) {
+
+    let tBB = mkSvgCircle({
+      svgContainer: staves[scrollBBsIx].svg,
+      cx: 0,
+      cy: scrBB_RADIUS,
+      r: scrBB_RADIUS,
+      fill: TEMPO_COLORS[scrollBBsIx],
+      stroke: 'white',
+      strokeW: 0
+    });
+    scrBBs.push(tBB);
+
+  } // END for (let scrollBBsIx = 0; scrollBBsIx < NUM_PLAYERS; scrollBBsIx++) {
+
+} //function makeBBcrvFollower() END
+//#endef Make Cursor Bouncy Ball Curve Follower
 
 
 //#endef BUILD WORLD
 
 //#ef WIPE/UPDATE/DRAW
 
-//###ef wipeScrollingCsrs
-function wipeScrollingCsrs() {
-  scrollingCursors.forEach((scrollingCsrSvgLine) => {
-    scrollingCsrSvgLine.setAttributeNS(null, 'display', 'none');
-  });
-}
-//###endef END wipeScrollingCsrs
 
 //###ef updateScrollingCsrs
-function updateScrollingCsrs(epochClock_MS) {
+function updateScrollingCsrs() {
 
-  let timelineMsIx = epochClock_MS % timelineMS.length;
-  let currScrollingCsrX = timelineMS[timelineMsIx];
+  let timelineFrameIx = FRAMECOUNT % timelineFRAMES.length;
+  let currScrollingCsrX = timelineFRAMES[timelineFrameIx].x;
 
-  for (let scrollingCsrIx = 0; scrollingCsrIx < scrollingCursors.length; scrollingCsrIx++) {
+  for (let playerIX = 0; playerIX < NUM_PLAYERS; playerIX++) {
 
-    scrollingCursors[scrollingCsrIx].setAttributeNS(null, 'x1', currScrollingCsrX);
-    scrollingCursors[scrollingCsrIx].setAttributeNS(null, 'x2', currScrollingCsrX);
-    scrollingCursors[scrollingCsrIx].setAttributeNS(null, 'display', 'yes');
+    scrollingCursors[playerIX].setAttributeNS(null, 'x1', currScrollingCsrX);
+    scrollingCursors[playerIX].setAttributeNS(null, 'x2', currScrollingCsrX);
+
+    scrBBs[playerIX].setAttributeNS(null, 'cx', currScrollingCsrX);
+    scrBBs[playerIX].setAttributeNS(null, 'cy', timelineFRAMES[timelineFrameIx].crvY); //updates y coordinate of BB from crvYcoords
 
   } // end for (let scrollingCsrIx = 0; scrollingCsrIx < scrollingCursors.length; scrollingCsrIx++)
 
 } // function updateScrollingCsrs() END
 //###endef updateScrollingCsrs
 
+
 //#endef WIPE/UPDATE/DRAW
 
 //#ef ANIMATION
+
 
 //#ef Animation Engine
 function animationEngine(timestamp) { //timestamp not used; timeSync server library used instead
@@ -285,10 +382,11 @@ function animationEngine(timestamp) { //timestamp not used; timeSync server libr
   while (cumulativeChangeBtwnFrames_MS >= MS_PER_FRAME) { //if too little change of clock time will wait until 1 animation frame's worth of MS before updating etc.; if too much change will update several times until caught up with clock time
 
     if (cumulativeChangeBtwnFrames_MS > (MS_PER_FRAME * FRAMERATE)) cumulativeChangeBtwnFrames_MS = MS_PER_FRAME; //escape hatch if more than 1 second of frames has passed then just skip to next update according to clock
-    wipe(tsNowEpochTime_MS);
-    update(tsNowEpochTime_MS);
-    draw(tsNowEpochTime_MS);
+    wipe();
+    update();
+    draw();
 
+    FRAMECOUNT++;
     cumulativeChangeBtwnFrames_MS -= MS_PER_FRAME; //subtract from cumulativeChangeBtwnFrames_MS 1 frame worth of MS until while cond is satisified
 
   } // while (cumulativeChangeBtwnFrames_MS >= MS_PER_FRAME) END
@@ -303,15 +401,13 @@ function animationEngine(timestamp) { //timestamp not used; timeSync server libr
 //#ef Wipe Function
 function wipe(epochClock_MS) {
 
-  // wipeScrollingCsrs();
-
 } // function wipe() END
 //#endef Wipe Function
 
 //#ef Update Function
-function update(epochClock_MS) {
+function update() {
 
-  updateScrollingCsrs(epochClock_MS);
+  updateScrollingCsrs();
 
 }
 //#endef Update Function
@@ -323,6 +419,7 @@ function draw(epochClock_MS) {
 //#endef Draw Function
 
 //#endef WIPE/UPDATE/DRAW
+
 
 //#endef ANIMATION
 
